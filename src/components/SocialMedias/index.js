584c392e615326c875da798810cd6e0ff5a67ee1
luckesilver <jacks2kar@gmail.com>
2022-02-19 13:55:56 -0300

    github

--- src/components/SocialMedias/index.js
+++ src/components/SocialMedias/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { FaGithub, FaLinkedin, FaInstagram, FaWhatsapp } from "react-icons/fa";
+import {FaLinkedin, FaInstagram, FaWhatsapp } from "react-icons/fa";
 
 import "./styles.css";
 
