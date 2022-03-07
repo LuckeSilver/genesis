2486ed455f9ccc0db5a226ab460fcde698fc5b78
luckesilver <jacks2kar@gmail.com>
2022-03-07 19:01:41 -0300

    Instagram apontando para genesis

--- src/components/SocialMedias/index.js
+++ src/components/SocialMedias/index.js
@@ -14,7 +14,7 @@ export default function SocialMedias() {
         >
           <FaLinkedin size={40} color="#c68c2c" />
         </a>
-        <a href="https://www.instagram.com//" target="blank">
+        <a href="https://www.instagram.com/genesistopografia/" target="blank">
           <FaInstagram size={40} color="#c68c2c" />
         </a>
         <a href="https://api.whatsapp.com/send?phone=5521981343917&text=Ol%C3%A1,%20gostaria%20de%20solicitar%20um%20or%C3%A7amento!" target="blank">
